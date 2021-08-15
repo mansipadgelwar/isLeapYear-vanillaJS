33fa92c9c9bb9ff84ec0a46d0f14483114244f9a
Mansi Padgelwar <74247233+mansipadgelwar@users.noreply.github.com>
2021-08-15 10:33:39 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+var txtInput = document.querySelector("#txt-input");
+var btnSubmit = document.querySelector("#btn-validate");
+var output = document.querySelector("#txt-output");
+btnSubmit.addEventListener("click",clickHandler);
+
+function clickHandler(){
+  var input = txtInput.value;
+  var userBirthDate = input.split("-");  
+  var YYYY = Number(userBirthDate[0]);
+  var MM = Number(userBirthDate[1]);
+  var DD = Number(userBirthDate[2]);
+  
+  if(input === "")
+  {
+    output.innerText = "Please enter the date.";
+  }
+  else
+  {
+    if(isLeapYear(YYYY)){
+      output.innerText = "🎊🎉Hurray!! You were born on a leap year. ";
+    }
+    else
+    {
+      output.innerText = "😥Ohh no!! You were not born on a leap year."
+    }
+  }  
+  
+}
+
+
+function isLeapYear(year) {
+ year = Number(year);
+ return (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0);
+}
